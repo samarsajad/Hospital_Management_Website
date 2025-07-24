825c0e963c09120fe68ef6a9e81fcfca823fd41d
Balraj Choure <balrajchoure09@email.com>
2025-07-24 23:54:21 +0530

    Improve homepage responsiveness and align product cards

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,20 +1,28 @@
 // Navbar.jsx
 import './styles.css';
 import logo from '../assets/logo-web.jpg';
-import { FaHome, FaInfoCircle, FaCog, FaUserDoctor, FaPhone } from 'react-icons/fa';
+import { FaHome, FaInfoCircle, FaCog, FaUserDoctor, FaPhone, FaBars } from 'react-icons/fa';
 import { FaUserMd } from 'react-icons/fa';
+import { useState } from 'react';
 
 
 
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header_container nav-h">
       <div className="img">
         <img id="rd-logo" style={{ width: '90px', height: '90px' }} src={logo} alt="Logo" />
       </div>
-      <nav className="nav_menu">
-        <ul className="nav_link vis-h">
+
+      <div className="hamburger" onClick={() => setIsOpen(!isOpen)}>
+        <FaBars />
+      </div>
+      
+      <nav className={`nav_menu ${isOpen ? 'open' : ''}`}>
+        <ul className="nav_link">
           <li><a href="#home"><FaHome /> Home</a></li>
           <li><a href="#about"><FaInfoCircle /> About</a></li>
           <li><a href="#service"><FaCog  /> Service</a></li>
