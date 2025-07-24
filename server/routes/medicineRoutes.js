253b885a95a3de324d8164a14c8337198783103f
Sneha <snehatomar1205@gmail.com>
2025-07-24 20:12:14 +0530

    Implement admin-only CRUD API for managing medicines

---
+++ server/routes/medicineRoutes.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  createMedicine,
+  getAllMedicines,
+  updateMedicine,
+  deleteMedicine
+} = require("../controllers/medicineController");
+
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const isAdmin = require("../middlewares/isAdmin");
+
+router.post("/", isAuthenticated, isAdmin, createMedicine);
+router.get("/", isAuthenticated, isAdmin, getAllMedicines);
+router.put("/:id", isAuthenticated, isAdmin, updateMedicine);
+router.delete("/:id", isAuthenticated, isAdmin, deleteMedicine);
+
+module.exports = router;
