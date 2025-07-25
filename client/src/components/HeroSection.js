10c9abf958a80836fd6af6a9fd6c6bd7f43dc8ad
codeX-Crafter <harshitabansal3737@gmail.com>
2025-07-25 20:36:41 +0530

    changes

--- client/src/components/HeroSection.js
+++ client/src/components/HeroSection.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import styles from './HeroSection.module.css';
-import pic1 from '../assets/pic5.webp';
+import React from "react";
+import styles from "./HeroSection.module.css";
+import pic1 from "../assets/pic5.webp";
 
 function HeroSection() {
   return (
@@ -12,7 +12,11 @@ function HeroSection() {
         </div>
         <div className={styles.heroText}>
           <p>
-            Welcome to <strong>MidCity Urology and General Nursing Home</strong>. Our dedicated team provides expert urological services and compassionate nursing care tailored to your needs. We prioritize your health and well-being, ensuring you receive top-quality care in a welcoming environment.
+            Welcome to <strong>MidCity Urology and General Nursing Home</strong>
+            . Our dedicated team provides expert urological services and
+            compassionate nursing care tailored to your needs. We prioritize
+            your health and well-being, ensuring you receive top-quality care in
+            a welcoming environment.
           </p>
         </div>
       </div>
@@ -24,7 +28,9 @@ function HeroSection() {
         </div>
         <div className={styles.messageText}>
           <p>
-            Thank you for choosing us as your healthcare partner. At MidCity, we strive for excellence in patient care, offering a blend of modern medical practices and a deeply human approach to healing.
+            Thank you for choosing us as your healthcare partner. At MidCity, we
+            strive for excellence in patient care, offering a blend of modern
+            medical practices and a deeply human approach to healing.
           </p>
         </div>
       </div>
