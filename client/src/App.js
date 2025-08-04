a613fc60a7a2ac047adc1783a94ca9c022ed8891
Tharunkunamalla <kunamallatharun701@gmail.com>
2025-08-05 00:20:26 +0530

    App.js issue solved

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,6 @@
-
 import React, {useState, useEffect} from "react";
 import "./components/styles.css";
 import {Routes, Route} from "react-router-dom";
-
-import React, { useState, useEffect } from "react";
-import "./components/styles.css";
-import { Routes, Route } from "react-router-dom";
 
 import HomePage from "./pages/HomePage";
 import PharmacyPage from "./pages/PharmacyPage";
@@ -13,13 +8,11 @@ import LabsPage from "./pages/LabsPage";
 import CheckupPage from "./pages/CheckupPage";
 import SurgeryPage from "./pages/SurgeryPage";
 
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import NotFoundPage from "./pages/NotFoundPage";
-import Login  from "./pages/Login";
+import Login from "./pages/Login";
 import Register from "./pages/Register";
-
 
 function App() {
   const [darkMode, setDarkMode] = useState(false);
@@ -31,7 +24,7 @@ function App() {
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<Login />} />
-      <Route path='register' element={<Register/>}/>
+      <Route path="register" element={<Register />} />
       <Route path="/services/pharmacy" element={<PharmacyPage />} />
       <Route path="/services/labs-diagnostics" element={<LabsPage />} />
       <Route path="/services/checkup" element={<CheckupPage />} />
