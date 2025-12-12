844e1dda8e6d1f3f4d48a514faf99822e92dcb90
Surya Prakash Kahar <sp2002rk@gmail.com>
2025-12-12 19:56:48 +0530

    Refactor Navbar and add Specialties section #192

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,27 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 // Import all home page sections
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Slider from "../components/Slider";
 import ServiceSection from "../components/ServiceSection";
+import SpecialtiesSection from "../components/SpecialtiesSection";
 import DoctorsSection from "../components/DoctorsSection";
 import AboutSection from "../components/AboutSection";
 import ContactSection from "../components/ContactSection";
 import FAQ from "../components/FAQ";
 
-
-const Homepage = ({ darkMode, setDarkMode }) => {
+const Homepage = () => {
+  const [darkMode, setDarkMode] = useState(false);
+  
   return (
     <div className={darkMode ? "dark-mode" : ""} id="home">
       <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
       <Slider darkMode={darkMode} />
       <AboutSection darkMode={darkMode} />
-      {/* <HeroSection darkMode={darkMode} /> */}
+      <SpecialtiesSection darkMode={darkMode} />
       <ServiceSection darkMode={darkMode} />
       <DoctorsSection darkMode={darkMode} />
       <ContactSection darkMode={darkMode} />
-      <FAQ darkMode={darkMode}/>
+      <FAQ darkMode={darkMode} />
       <Footer />
     </div>
   );
